6cf7c12d35e32f17fd8360407ae2f6489077a41d
Dennis Jutemark <dennis@jutemark.net>
2026-04-19 10:27:54 +0200

    Reset spring air jumps

--- scripts/spring.gd
+++ scripts/spring.gd
@@ -1,10 +1,8 @@
 extends Area2D
 
 @export var jump_strength: float = 200
-#@export var reset_air_jumps: bool = false
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is Player:
 		body.do_jump(Vector2.UP * jump_strength)
-		#if reset_air_jumps:
-			#body.reset_air_jumps()
+		body.reset_air_jumps()
