bd0a5cfc6a11723bfa6d81d860293859043d1ab9
Dennis Jutemark <dennis@jutemark.net>
2026-04-19 16:33:09 +0200

    Add game timer

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -1,6 +1,7 @@
 extends CanvasLayer
 
 @export var gui_key: PackedScene
+@export var allowed_time_seconds: float = 60 * 10
 
 var current_signal_uses: int:
 	get:
@@ -22,13 +23,28 @@ var collected_keys: int = 0:
 		_add_keys_gui(keys_to_activate)
 		collected_keys = value
 
+var _elapsed_seconds := 0.0
 
 static func to_bit_string(value: int) -> String:
 	return String.num_int64(value, 2)
 
 
+func _process(delta: float) -> void:
+	_elapsed_seconds += delta
+	var seconds_left := allowed_time_seconds - _elapsed_seconds
+	
+	%Timer.text = seconds_to_display(seconds_left)
+
+
+static func seconds_to_display(total_seconds: float) -> String:
+	var full_seconds := (floor(total_seconds) as int) % 60
+	var minutes: int = floor(total_seconds) / 60
+	var hundredths := (floor(total_seconds * 100.0) as int) % 100
+	return "%d:%02d.%02d" % [minutes, full_seconds, hundredths]
+
+
 func _update_uses_text() -> void:
-	%NumUses.text = "Signaler uses: %d / %d" % [current_signal_uses, max_signal_uses]
+	%NumUses.text = "Signals: %d / %d" % [current_signal_uses, max_signal_uses]
 
 func _add_keys_gui(keys: int) -> void:
 	print("keys_to_activate = ", to_bit_string(keys))
@@ -40,4 +56,3 @@ func _add_keys_gui(keys: int) -> void:
 			var k = gui_key.instantiate()
 			(k as CanvasItem).modulate = color
 			%KeysContainer.add_child(k)
-		
