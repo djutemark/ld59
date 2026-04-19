84579be07ac03f6171fc2886b00b7831566e2727
Dennis Jutemark <dennis@jutemark.net>
2026-04-19 12:46:20 +0200

    Create teleporter node

---
+++ scripts/teleporter.gd
@@ -0,0 +1,14 @@
+class_name Teleporter
+extends Area2D
+
+@export var teleport_target: Node2D
+
+
+func _ready() -> void:
+	self.body_entered.connect(_on_body_entered)
+	
+
+func _on_body_entered(other: Node2D) -> void:
+	if other is Player:
+		other.respawn_position = teleport_target
+		other.respawn()
