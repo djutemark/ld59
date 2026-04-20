9e5f425a77463514a0db0a2b263bd61543090bb6
Dennis Jutemark <dennis@jutemark.net>
2026-04-20 19:30:58 +0200

    Animate spring

--- scripts/spring.gd
+++ scripts/spring.gd
@@ -2,8 +2,19 @@ extends Area2D
 
 @export var jump_strength: float = 200
 
+var anim: AnimatedSprite2D
+
+func _ready() -> void:
+	anim = %Anim as AnimatedSprite2D
+	anim.sprite_frames.set_animation_loop("jump", false)
+	anim.sprite_frames.set_animation_loop("idle", false)
+
+
 func _on_body_entered(body: Node2D) -> void:
 	var p := body as Player
 	if p:
 		p.do_jump(Vector2.UP * jump_strength)
 		p.reset_air_jumps()
+		anim.play("jump")
+		await anim.animation_finished
+		anim.play("idle")
